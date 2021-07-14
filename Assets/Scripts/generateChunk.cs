50ece6594fe743854c918e81641b457a399b6171
Thom Esgate <34862004+ThomEsgate@users.noreply.github.com>
2021-07-14 19:23:29 +0100

    Updated world gen to include dark forest
    + also added the startings of a block class

--- Assets/Scripts/generateChunk.cs
+++ Assets/Scripts/generateChunk.cs
@@ -3,24 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public enum Biome { 
-    FOREST_THIN, 
-    FOREST_THICK, 
-    FOREST_PLAINS
+
+
+public enum Biome { //Variants of the NORMAL FOREST Biome
+    FOREST_THIN,        // 0
+    FOREST_PLAINS,      // 1
+    FOREST_THICK,       // 2
+
+    FOREST_THIN_DARK,   // 3
+    FOREST_PLAINS_DARK, // 4
+    FOREST_THICK_DARK,  // 5
+    FOREST_BRAMBLES     // 6
 }
 
-
+/*
+ * CHUNK FOR NORMAL FOREST BIOME
+ * MAKE A BLOCK OBEJCT THAT INHERITS GAMEOBJECT, SO U CAN GIVE IT IDs ??
+ */
 
 public class generateChunk : MonoBehaviour
 {
-    // ((((((((((((((((((((((((********************************************** MAKE A BLOCK OBEJCT THAT INHERITS GAMEOBJECT, SO U CAN GIVE IT IDs
-    public GameObject DirtTile;
-    public GameObject GrassTile;
-    public GameObject GrassDarkTile;
-    public GameObject StoneTile;
-    public GameObject WoodTile;
-    public GameObject SandTile;
-
+    //Variables needed in every chunk type
     private generateChunks genChunks;
 
     public int width;
@@ -34,6 +37,18 @@ public class generateChunk : MonoBehaviour
 
     public Biome chunkBiome;
     public System.Random rand;
+    public int chunkID; //How far along the chunk is in the world
+    public bool isDark; //Determines if you're in the dark forest or not
+
+
+    //Chunk specific tiles
+    public GameObject DirtTile;
+    public GameObject GrassTile;
+    public GameObject StoneTile;
+    public GameObject WoodTile;
+    public GameObject SandTile;
+
+    
 
     //trees
     private int chance_tree;
@@ -43,42 +58,76 @@ public class generateChunk : MonoBehaviour
     private int holes; //no of tiles to go through and add 1 block holes to - e.g. if its 7, (o = block, x = hole) oxxoxoo
 
     // Start is called before the first frame update
-    void Start()
-    {
-        rand = new System.Random(seed);//!!!!!!!!!!!
-        //int biomeChoice = rand.Next(3);
-        //Debug.Log(biomeChoice);
-        int biomeChoice = 1;
+    void Start(){
+       
         genChunks = FindObjectOfType<generateChunks>();
+        rand = genChunks.rand;//Use the same random generator in the main generating script
+        
 
-        //Picking out of 3 possible surface biomes
-        switch (biomeChoice){
-            case 0: chunkBiome = Biome.FOREST_PLAINS;break;
-            case 1: chunkBiome = Biome.FOREST_THIN;  break;
-            case 2: chunkBiome = Biome.FOREST_THICK; break;
-        }
+        if (!isDark){
+            int biomeChoice = rand.Next(3);
+            
+            //Debug.Log(biomeChoice);
+            //int biomeChoice = 2 ;
+            //Picking out of possible surface biomes, which determines how they're generated
+            switch (biomeChoice){
+                case 0: chunkBiome = Biome.FOREST_PLAINS;break;
+                case 1: chunkBiome = Biome.FOREST_THIN;  break;
+                case 2: chunkBiome = Biome.FOREST_THICK; break;
+            }
 
-        genChunks.chunkBiome = chunkBiome;
+            genChunks.chunkBiome = chunkBiome;
 
-        //Giving the surface biomes their own stats
-        if (chunkBiome == Biome.FOREST_THICK) {
-            tree_max_height = 12;
-            chance_tree = 5; //1 in 5
-            smoothness = 48;
-            heightMultiplier = 64;
+            //Giving the surface biomes their own stats
+            if (chunkBiome == Biome.FOREST_THICK) {
+                tree_max_height = 12;
+                chance_tree = 8; //1 in 8
+                smoothness = 48;
+                heightMultiplier = 48;
+            }
+            else if (chunkBiome == Biome.FOREST_THIN) {
+                tree_max_height = 8;
+                chance_tree = 16; //1 in 16
+                smoothness = 56;
+                heightMultiplier = 32;
+            }
+            else if (chunkBiome == Biome.FOREST_PLAINS) {
+                tree_max_height = 0; //No trees
+                smoothness = 64;
+                    heightMultiplier = 32;
+            }
         }
-        else if (chunkBiome == Biome.FOREST_THIN) {
-            tree_max_height = 8;
-            chance_tree = 16; //1 in 16
-            smoothness = 56;
-            heightMultiplier = 32;
-        }
-        else if (chunkBiome == Biome.FOREST_PLAINS) {
-            tree_max_height = 0;
-            smoothness = 64;
-            heightMultiplier = 32;
-        }
+        else{ //Dark Biomes
+            int biomeChoice = rand.Next(3,6);
+            
+            switch (biomeChoice){
+                case 3: chunkBiome = Biome.FOREST_PLAINS_DARK;  break;
+                case 4: chunkBiome = Biome.FOREST_THIN_DARK;    break;
+                case 5: chunkBiome = Biome.FOREST_THICK_DARK;   break;
+            }
 
+            //Giving the surface biomes their own stats
+            if (chunkBiome == Biome.FOREST_THICK)
+            {
+                tree_max_height = 12;
+                chance_tree = 8; //1 in 8
+                smoothness = 48;
+                heightMultiplier = 48;
+            }
+            else if (chunkBiome == Biome.FOREST_THIN)
+            {
+                tree_max_height = 8;
+                chance_tree = 16; //1 in 16
+                smoothness = 56;
+                heightMultiplier = 32;
+            }
+            else if (chunkBiome == Biome.FOREST_PLAINS)
+            {
+                tree_max_height = 0; //No trees
+                smoothness = 64;
+                heightMultiplier = 32;
+            }
+        }
         Generate();
     }
 
@@ -87,7 +136,7 @@ public class generateChunk : MonoBehaviour
         int offset = 0;
         for (int i = 0; i < width; i++){
             
-            //Stop problem of chunks in different biomes being at the wrong heights, so add an offset (bodge!)
+            //Stop problem of chunks in different biomes being at the wrong heights, so add an offset (bodge!??)
             if (i == 0) {
                 h = Mathf.RoundToInt(Mathf.PerlinNoise(seed, (i + transform.position.x * 0.5f) / smoothness) * heightMultiplier) + offset + heightAddition;
  
@@ -97,11 +146,6 @@ public class generateChunk : MonoBehaviour
                 else{
                     offset = genChunks.oldHeight - h;
                 }
-
-                Debug.Log("oldHeight: " + genChunks.oldHeight);
-                Debug.Log("h: " + h);
-                Debug.Log("offset bef: " + offset);
-                Debug.Log("oldHeight before: " + genChunks.oldHeight);
             }
 
             h = Mathf.RoundToInt(Mathf.PerlinNoise(seed, (i + transform.position.x * 0.5f) / smoothness) * heightMultiplier) + offset + heightAddition;
@@ -130,40 +174,33 @@ public class generateChunk : MonoBehaviour
             genChunks.oldHeight = h;
         }
 
-        Debug.Log("oldHeightAfter: " + genChunks.oldHeight);
-        Debug.Log("");
-        Debug.Log("");
-        Debug.Log("");
+        //Debug.Log("oldHeightAfter: " + genChunks.oldHeight);
+        //Debug.Log("");
+        //Debug.Log("");
+        //Debug.Log("");
     }
 
     /*
-     * Used for 
+     * Used for making the overground blocks
      * 
+     * @params: Biome Enum, random number generator, how far along in the chunk it is 
      * 
      */
-    private void fillBlocks(Biome chunkBiome, System.Random rand, int i){  //Biome Enum, random number generator, how far along in the chunk it is
+    private void fillBlocks(Biome chunkBiome, System.Random rand, int i){  
         for (int j = -64; j < h; j++)
         {
             GameObject selectedTile;
-            if (j < h - 1)
-            {
-                selectedTile = DirtTile;
-            }
-            else
-            {
-                if (chunkBiome == Biome.FOREST_THICK)
-                {
-                    selectedTile = GrassDarkTile;
-                }
-                else
-                {
-                    selectedTile = GrassTile;
-                }
+            if (j >= h - 1){ //ON surface
+                selectedTile = GrassTile;
 
-                if (rand.Next(chance_tree) == 1)
-                {
-                    makeTree(i, j, tree_max_height);
+                if (rand.Next(chance_tree) == 1){
+                    genChunks.makeTree(i + chunkID * width, j, tree_max_height, WoodTile);
                 }
+                
+            }
+
+            else{
+                selectedTile = DirtTile;
             }
             GameObject newTile = Instantiate(selectedTile, new Vector3(i, j), Quaternion.identity);
             newTile.transform.parent = this.gameObject.transform;
@@ -171,17 +208,6 @@ public class generateChunk : MonoBehaviour
         }
     }
 
-    private void makeTree(int i, int j, int tree_max_height){
-        //Debug.Log("placing tree!!!!!!");
-        int treeHeight = rand.Next(tree_max_height/2, tree_max_height);
-        //Debug.Log("treeHeight: " + treeHeight);
-        for (int k = 0; k < treeHeight; k++){
-            //Debug.Log("placing at: " + i + ", " + (j + k + 1) );
-            GameObject tree = Instantiate(WoodTile, new Vector3(i, j + k + 1), Quaternion.identity);
-            tree.transform.parent = this.gameObject.transform;
-            tree.transform.localPosition = new Vector3(i, j + k + 1);
-        }
-
-    }
+    
 
 }
--- Assets/Scripts/generateChunks.cs
+++ Assets/Scripts/generateChunks.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class generateChunks : MonoBehaviour {
 
     public GameObject chunk;
+    public GameObject chunk_dark;
     int chunkWidth;
-    public int numChunks;
+    //Make these constants later
+    public int numChunksTotal;
+    public int numChunksForest; //Number of chunks to generate of forest    
+    public int numChunksForest_dark; //Number of chunks to generate of forest    
+    public ArrayList biomeTypeArray = new ArrayList();
+
+
+    public int lastX;
     int seed;
     public int oldHeight;
     public Biome chunkBiome;
@@ -24,14 +32,56 @@ public class generateChunks : MonoBehaviour {
         Generate();    
     }
 
-    // Update is called once per frame
+    void GenerateChunkOfType(GameObject chunkType, int lastX_param, int chunkID) //chunkType = biome for 
+    {
+        lastX = lastX_param;
+        GameObject newChunk = Instantiate(chunkType, new Vector3(lastX + chunkWidth, 0f), Quaternion.identity) as GameObject;
+        newChunk.GetComponent<generateChunk>().seed = seed;
+        newChunk.GetComponent<generateChunk>().rand = rand;
+        newChunk.GetComponent<generateChunk>().chunkID = chunkID;
+    }
+
+    /*Main method for generating chunks in the world
+     * 
+     * 
+     */
     void Generate(){
-        int lastX = -chunkWidth;
-        for (int i = 0; i < numChunks; i++){
-            GameObject newChunk = Instantiate(chunk, new Vector3(lastX + chunkWidth, 0f), Quaternion.identity) as GameObject;
-            newChunk.GetComponent<generateChunk>().seed = seed;
-            newChunk.GetComponent<generateChunk>().rand = rand;
+        lastX = -chunkWidth; //lastX = last starting position, all subsiquent chunks start fro
+
+        //Generate dark forest
+        for (int i = 0; i < numChunksForest_dark; i++){
+            GenerateChunkOfType(chunk_dark, lastX, i);
+            lastX += chunkWidth;
+        }
+
+        //Generate forest
+        for (int i = 0; i < numChunksForest; i++){
+            GenerateChunkOfType(chunk, lastX, i + numChunksForest_dark); // +numChunksForest_dark because that no. of chunks have already been generated, so you cant start counting the ID from 0 again or there will be 2 chunks with the same ID
             lastX += chunkWidth;
         }
     }
+
+
+    /*
+     * Making a tree, can change the type of wood the tree is made of
+     * local x coordinate (with respect to chunk), local y coordinate (with respect to chunk), max height of tree, type of wood
+     */
+    public void makeTree(int i, int j, int tree_max_height, GameObject WoodTile){
+        //Check to see if possible to place tree
+        //
+
+        int treeHeight = rand.Next(tree_max_height / 2, tree_max_height);
+
+        for (int k = 0; k < treeHeight; k++) //places blocks to form a tree, k being the height of the block being placed
+        {
+            //Debug.Log("placing at: " + i + ", " + (j + k + 1) );
+            GameObject tree = Instantiate(WoodTile, new Vector3(i, j + k - 1), Quaternion.identity);
+            tree.transform.parent = this.gameObject.transform;
+            tree.transform.localPosition = new Vector3(i, j + k + 1);
+        }
+
+    }
+
+
+
 }
